6371aee7ea87d8445d411e06de8003e0cb134c57
yannJu <yann_ju@naver.com>
2023-03-22 16:30:59 +0900

    setValue 사용

--- ex05_led_with_object/ex05_led_with_object.ino
+++ ex05_led_with_object/ex05_led_with_object.ino
@@ -12,13 +12,21 @@ void loop() {
   int n;
 
   for (n = 0; n < 4; n++) {
-    if (n == out_no) leds[n].on();
-    else leds[n].off();
+    // if (n == out_no) leds[n].on();
+    // else leds[n].off();
+
+    // ====================
     // 혹은 toggle 을 이용하여 아래와 같이도 구현 가능
-    // toggle 사용시 for문 밖의 두줄은 사용 안함
+    //    단, toggle 사용시 for문 밖의 두줄은 사용 안함
+    // ====================
     // leds[n].toggle();
     // delay(1000);
     // leds[n].toggle();
+    
+    // ====================
+    // 혹은 setValue 를 이용하여 구현 가능
+    // ====================
+    leds[n].setValue(n == out_no);
   }
 
   ++out_no %= 4;
