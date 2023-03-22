bca8488bd4e8816abfff9e9305f0b1e67e4fc684
yannJu <yann_ju@naver.com>
2023-03-22 17:49:33 +0900

    ex06_SWTICH 센서 실습

--- SWITCH(ex06-)/ex06_switch/ex06_switch.ino
+++ SWITCH(ex06-)/ex06_switch/ex06_switch.ino
@@ -1,9 +1,21 @@
+#include <LED.h>
+
+const int pu_sw_pin = 2;
+LED blue_led(4);
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(pu_sw_pin, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  boolean pu_sw;
 
+  // default 로 switch 를 누르지 않으면 I/O 에 high
+  //    즉 pu_sw = 1
+  // pu_sw 를 setValue 로 하면 버튼을 누르지 않을 때 LED = High 
+  // 따라서 '!' 를 이용하여 풀다운 방식과 같이 구현
+  pu_sw = digitalRead(pu_sw_pin);
+  blue_led.setValue(!pu_sw);
 }
